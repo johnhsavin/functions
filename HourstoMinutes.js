a9013dbcc8cc613e4c42cb92d0c86844165650d4
John Savin <johnsavin@Johns-Air.attlocal.net>
2023-10-04 22:42:03 -0400

    areaoftriangle challenge

--- HourstoMinutes.js
+++ HourstoMinutes.js
@@ -1,6 +1,6 @@
 function hours_to_minutes(hours){
-    const hours_to_minutes = hours * 60 
-    return hours_to_minutes
+    const minutes = hours * 60 
+    return minutes
 }
 
 console.log(hours_to_minutes(5))
---
+++ areaoftriangle.js
@@ -0,0 +1,8 @@
+function areaOfATriangle(base, height){
+
+const areaOfATriangle = .5 * base * height
+
+return areaOfATriangle 
+}
+
+console.log(areaOfATriangle(3, 5))
